e582c0440ed4343757d1ab7cc5363518ad9701f4
ivisondsb <ivisonk0@gmail.com>
2024-10-28 19:20:41 -0300

    create scripts.js

---
+++ scripts.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const addTaskButton = document.getElementById("addTaskButton");
+
+    addTaskButton.addEventListener("click", () => {
+        const title = document.getElementById("taskTitle").value.trim();
+        let description = document.getElementById("taskDescription").value;
+        if (description === "") {
+            description = null
+        }
+        if (title) {
+            console.log(`Tarefa Adicionada!\n\nTarefa: ${title}\nDescrição: ${description}`);
+        } else {
+            alert("Por favor, preencha o título da sua tarefa.");
+        }
+    });
+});
