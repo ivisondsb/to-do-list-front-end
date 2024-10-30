2ddac8b57c80f43e2574157f0dc69a3244134cd0
ivisondsb <ivisonk0@gmail.com>
2024-10-30 19:18:58 -0300

    update script.js to send data to the API

--- scripts.js
+++ scripts.js
@@ -8,9 +8,35 @@ document.addEventListener("DOMContentLoaded", () => {
             description = null;
         }
         if (title) {
-            console.log(`Tarefa Adicionada!\n\nTarefa: ${title}\nDescrição: ${description}`);
+            const newTask = {
+                title: title,
+                description: description
+            };
+            fetch("http://localhost:8080/tasks", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(newTask)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    document.getElementById("taskTitle").value = "";
+                    document.getElementById("taskDescription").value = "";
+
+                    addTaskToList(data);
+                })
         } else {
             alert("Por favor, preencha o título da sua tarefa.");
         }
     });
+
+    function addTaskToList(task) {
+        const taskList = document.getElementById("taskList");
+        const taskItem = document.createElement("div");
+        taskItem.classList.add("task-item");
+        taskItem.innerHTML = `
+            <h3>${task.title}</h3>
+            <p>${task.description}</p>
+        `;
+        taskList.appendChild(taskItem);
+    }
 });
